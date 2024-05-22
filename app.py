f19f90f365b31c4a4669734b4687dcc077f5c064
Spetozz <99469035+Spetozz@users.noreply.github.com>
2024-05-22 19:13:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import g4f
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,13 +22,17 @@ def chat():
     input = msg
     return get_Chat_response(input)
 
-def get_Chat_response(promt: str) -> str:
+# gemini
+# gpt-3.5-turbo
+# gpt-4o
+def get_Chat_response(promt):
     response = g4f.ChatCompletion.create(
-        model="gpt-4o",
+        model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": promt}],
     )
     print(response)
     return response
+
 
 if __name__ == '__main__':
     app.run()
